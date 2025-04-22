86dec5af7ff83813f39f6d2859c4170032a54202
iwashi623 <wonderland.7878@gmail.com>
2025-04-22 23:47:57 +0900

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 bundle install
+# /storageディレクトリを作成
+mkdir -p /storage
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
